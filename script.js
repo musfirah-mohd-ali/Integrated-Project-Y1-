df5c27f831fa77b8607b9741e93772793a666158
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-02-02 18:40:16 +0800

    made hte hamburger close once hte user clicks out

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
+/*HAMBURGER MENU*/ 
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".hamburger-menu");
     const navLinks = document.querySelector(".nav-link");
 
-    menuButton.addEventListener("click", function () {
-        if (window.innerWidth <= 768) {
-            navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+    menuButton.addEventListener("click", function (event) {
+        event.stopPropagation();
+        navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+    });
+    document.addEventListener("click", function (event) {
+        if (!navLinks.contains(event.target)&& !menuButton.contains(event.target)){
+            navLinks.style.display = "none";
         }
     });
 });
