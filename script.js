2a956f328e08c3a2689d5dbaacca1842c6b3fd6a
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-02-02 18:19:19 +0800

    tried to fix the js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-/*FOR HAMBURGER MENU*/
-document.addEventListener('DOMContentLoaded',function(){
+document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".hamburger-menu");
-    const navLinks = document.querySelector(".nav-links");
+    const navLinks = document.querySelector(".nav-link");
 
-    menuButton.addEventListener("click",function (){
-        if (window.innerWidth<=768){
-            navLinks.computedStyleMap.display = navLinks.style.display === "flex" ? "none" : "flex";
+    menuButton.addEventListener("click", function () {
+        if (window.innerWidth <= 768) {
+            navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
         }
     });
 });
