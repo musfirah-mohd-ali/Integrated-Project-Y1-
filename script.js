313cf62efaee4a78c0c8a0f8af2374cc321ffef0
musfirah-mohd-ali <s10270156@connect.np.edu.sg>
2025-02-09 16:43:57 +0800

    fixed the media nav bar issue and centralised the logo in media view

--- script.js
+++ script.js
@@ -2,12 +2,19 @@
 document.addEventListener("DOMContentLoaded", function () {
     const menuButton = document.querySelector(".hamburger-menu");
     const navLinks = document.querySelector(".nav-link");
-    menuButton.addEventListener("click", function () {
-        if (window.innerWidth <= 768) {
-            navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+
+    menuButton.addEventListener("click", function (event) {
+        event.stopPropagation();
+        navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
+    });
+
+    document.addEventListener("click", function (event) {
+        if (!navLinks.contains(event.target) && !menuButton.contains(event.target)) {
+            navLinks.style.display = "none";
         }
     });
 });
+
 
 // FUNCTION TO TOGGLE BETWEEN SIGN UP AND LOG IN
 document.addEventListener("DOMContentLoaded", function () {
